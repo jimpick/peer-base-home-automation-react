68b96ce0f1173b3bc53a918ee1a7df90ad8b8bd7
Jim Pick <jim@backblend.com>
2019-02-07 23:35:32 -0800

    Get basic demo working

--- src/PeerBaseLights.js
+++ src/PeerBaseLights.js
@@ -1,30 +1,120 @@
 import React, { useState, useEffect } from 'react';
+import PeerBase from 'peer-base'
+import IPFSRepo from 'ipfs-repo'
+import MemoryDatastore from 'interface-datastore'
+import { Set } from 'immutable'
 
-export default function Example() {
-  // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
-  const [count2, setCount2] = useState(0);
+const rooms = [
+  'entrance',
+  'living room',
+  'kitchen',
+  'bedroom',
+  'den',
+  'bathroom'
+];
+
+async function startPeerBase () {
+  const app = PeerBase('lights-demo', {
+    ipfs: {
+      repo: new IPFSRepo('ipfs', {
+        lock: 'memory',
+        storageBackends: {
+          root: MemoryDatastore.MemoryDatastore,
+          blocks: MemoryDatastore.MemoryDatastore,
+          keys: MemoryDatastore.MemoryDatastore,
+          datastore: MemoryDatastore.MemoryDatastore
+        }
+      }),
+      swarm: ['/dns4/rendezvous.jimpick.com/tcp/9091/wss/p2p-websocket-star'],
+      // swarm: ['/dns4/ws-star1.par.dwebops.pub/tcp/443/wss/p2p-websocket-star'],
+      bootstrap: [
+        '/dns4/ams-1.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLer265NRgSp2LA3dPaeykiS1J6DifTC88f5uVQKNAd',
+        '/dns4/lon-1.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLMeWqB7YGVLJN3pNLQpmmEk35v6wYtsMGLzSr5QBU3',
+        '/dns4/sfo-3.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLPppuBtQSGwKDZT2M73ULpjvfd3aZ6ha4oFGL1KrGM',
+        '/dns4/sgp-1.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLSafTMBsPKadTEgaXctDQVcqN88CNLHXMkTNwMKPnu',
+        '/dns4/nyc-1.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLueR4xBeUbY9WZ9xGUUxunbKWcrNFTDAadQJmocnWm',
+        '/dns4/nyc-2.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLV4Bbm51jM9C4gDYZQ9Cy3U6aXMJDAbzgu2fzaDs64',
+        '/dns4/node0.preload.ipfs.io/tcp/443/wss/ipfs/QmZMxNdpMkewiVZLMRxaNxUeZpDUb34pWjZ1kZvsd16Zic',
+        '/dns4/node1.preload.ipfs.io/tcp/443/wss/ipfs/Qmbut9Ywz9YEDrz8ySBSgWyJk41Uvm2QJPhwDJzJyGFsD6'
+      ]
+    }
+  });
+
+  console.log('Starting...');
+  await app.start();
+  const collaboration = await app.collaborate('lights', 'rwlwwset');
+  return [ app, collaboration ];
+}
+
+async function stopPeerBase (app, collaboration) {
+  await collaboration.stop();
+  await app.stop(true); // stop ipfs
+}
+
+
+export default function PeerBaseLights () {
+  const [roomsSet, setRoomsSet] = useState(new Set());
+  const [roomsShared, setRoomsShared] = useState();
+  const [peers, setPeers] = useState();
 
   useEffect(() => {
-    console.log('Jim1');
-    let count3 = count2;
-    const intervalId = setInterval(() => {
-      console.log('Jim2');
-      count3 += 1;
-      setCount2(count3);
-    }, 1000);
-    return function cleanup () {
-      console.log('Jim3');
-      clearInterval(intervalId);
-    }
+    startPeerBase().then(([ app, collaboration ]) => {
+      collaboration.on('state changed', update);
+      collaboration.on('membership changed', updatePeers);
+      setRoomsShared(collaboration.shared);
+      return cleanup;
+
+      function update () {
+        console.log('Jim1', collaboration.shared.value());
+        setRoomsSet(Set(collaboration.shared.value()));
+      }
+
+      function updatePeers (peers) {
+        console.log('Jim2 peers', peers);
+        setPeers(Set(peers));
+      }
+
+      function cleanup () {
+        collaboration.removeListener('state changed', update);
+        collaboration.removeListener('membership changed', updatePeers);
+        stopPeerBase(app, collaboration);
+        console.log('Cleanup');
+      }
+    })
   }, []);
+
+  if (!roomsShared) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <div>
-      <p>You clicked {count * 10 + count2} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
+      <p>Peers:</p>
+      <ul>
+        { peers && peers.map(peer => <li key={peer}>{peer}</li>) }
+      </ul>
+      <p>Rooms:</p>
+      <ul>
+        {
+          rooms.map(room => (
+            <li key={room}>
+              <button onClick={toggle(room)}>Toggle</button>
+              {room}: {roomsSet.has(room) ? 'On' : 'Off'}
+            </li>
+          ))
+        }
+      </ul>
     </div>
   );
+
+  function toggle (room) {
+    return () => {
+      console.log('Toggle:', room);
+      if (roomsSet.has(room)) {
+        roomsShared.remove(Date.now(), room);
+      } else {
+        roomsShared.add(Date.now(), room);
+      }
+    }
+  }
 }
